61ff666b78f22930b8f50ba8084080f4dc6861db
casssi <61418715+casssi@users.noreply.github.com>
2021-05-06 22:35:06 +0200

    Create get_relation.py

---
+++ get_relation.py
@@ -0,0 +1,43 @@
+# Librairy to draw the graph
+import re
+import pandas as pd
+import bs4
+import requests
+import spacy
+from spacy import displacy
+nlp = spacy.load('en_core_web_sm')
+
+from spacy.matcher import Matcher 
+from spacy.tokens import Span 
+
+import networkx as nx
+
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+pd.set_option('display.max_colwidth', 200)
+#%matplotlib inline
+
+
+# Function to define the relation possible
+def get_relation(sent):
+
+  doc = nlp(sent)
+
+  # Matcher class object 
+  matcher = Matcher(nlp.vocab)
+
+  #define the pattern 
+  pattern = [{'DEP':'ROOT'}, 
+            {'DEP':'prep','OP':"?"},
+            {'DEP':'agent','OP':"?"},  
+            {'POS':'ADJ','OP':"?"}] 
+
+  matcher.add("matching_1", [pattern]) 
+
+  matches = matcher(doc)
+  k = len(matches) - 1
+
+  span = doc[matches[k][1]:matches[k][2]] 
+
+  return(span.text)
